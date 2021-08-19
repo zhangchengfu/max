cccc88ca00536241a40c3aa62e33cf8d5e6e252a
zhangchengfu <zhangchengfu@zhongdihang.com>
2021-08-19 09:45:03 +0800

    20210819 ms 增加网关、diners

--- ms-parent/README.md
+++ ms-parent/README.md
@@ -1,1 +1,5 @@
 ### 基于Spring Cloud的微服务项目架构,Eureka作为注册中心，Gateway作为网关
+
+### 启动顺序RegistryApplication -> 网关 GatewayApplication -> DinersApplication
+
+
---
+++ ms-parent/ms-diners/src/main/java/com/laozhang/diners/DinersApplication.java
@@ -0,0 +1,13 @@
+package com.laozhang.diners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinersApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DinersApplication.class, args);
+    }
+}
